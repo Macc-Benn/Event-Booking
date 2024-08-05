aadea042b411a2c0b386ca4c55a2862fc855ee5f
Mfoniso Ndaobong <mfoniso.ndaobong@gmail.com>
2024-08-05 14:32:38 +0100

    bug fixes

--- checkout/checkoutscript.js
+++ checkout/checkoutscript.js
@@ -83,5 +83,7 @@ var x = setInterval(function() {
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "EXPIRED";
+      
+
     }
 }, 1000);
